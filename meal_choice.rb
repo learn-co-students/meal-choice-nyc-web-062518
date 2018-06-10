4819182cbbd4ab5dcbee47b5766f9230b585f1d8
Your Name <you@example.com>
2018-06-10 10:59:49 -0400

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
-# passed into it and defaults to meat.
+def meal_choice(name = "meat")
+# def tells us we are defining a method called meal_choice which has an argument called name which we set to a defualt value "meat"
+"#{name}"
+# the body of our method tells what we are doing which is just giving us the name of the arugment passed through the method
+end
+# we have ended our method
